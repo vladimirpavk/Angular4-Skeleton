2179f677f6fee17b6bce09e752b3a17e3c8ae98c
Korisnik <vladimirpavk@gmail.com>
2017-04-11 23:31:32 +0200

    Working 4.0.1

--- client/app/app.module.ts
+++ client/app/app.module.ts
@@ -2,14 +2,14 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent }   from './app.component';
-import { CommonComponentsModule } from 'commoncomponents';
-import { Ng2Bs3ModalModule } from 'modalcomponent';
+//import { CommonComponentsModule } from 'commoncomponents';
+//import { Ng2Bs3ModalModule } from 'modalcomponent';
 
 @NgModule({
   imports:      [ 
                   BrowserModule,
-                  CommonComponentsModule,
-                  Ng2Bs3ModalModule 
+                  //CommonComponentsModule,
+                  //Ng2Bs3ModalModule 
                 ],
   declarations: [ AppComponent ],
   bootstrap:    [ AppComponent ]
